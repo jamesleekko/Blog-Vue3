00698e26b20803f84a06268f38daff4727ba549a
jamesleekko <jamesleecode@gmail.com>
2022-07-12 15:52:50 +0800

    fix index banner size and header left size

--- src/components/paths/Index.vue
+++ src/components/paths/Index.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useDark, useToggle } from '@vueuse/core'
 import store from '~/assets/plugins/vuex/vuex'
-// import $ from 'jquery'
+import banner1 from '~/assets/images/gallery/index/index-banner1.jpg'
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
@@ -10,15 +10,17 @@ const toggleDark = useToggle(isDark)
 const count = ref(1)
 
 const curPictureNum = Math.floor(Math.random() * (store.state.galleryTotal - 1)) + Number(1);
-const curPicture = `../../../assets/images/gallery/index-banner${curPictureNum}.jpg`
-setTimeout(() => {
+const curPictureUrl = new URL('/assets/images/gallery/index/index-banner' + curPictureNum + '.jpg', import.meta.url).href;
+
+onMounted(() => {
   console.log('show gallery ele', $('.index-gallery'))
-  $('.index-gallery').css('background-image', `url(${curPicture})`)
-}, 1000);
+  $('.index-gallery').css('background-image', `url(${banner1})`)
+})
 </script>
 
 <template>
   <div class="index-gallery">
+    <div style="height: 20px"></div>
     <p>
       Recommended IDE setup:
       <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
@@ -44,6 +46,7 @@ setTimeout(() => {
       <code>components/HelloWorld.vue</code> to test hot module replacement.
     </p>
   </div>
+  <div style="height: 40px;">fdsjfiods</div>
 </template>
 
 <style lang="scss" scoped>
