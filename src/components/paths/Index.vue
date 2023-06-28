101bf6e13dbc0f9530c562d0fb7f31c782986fdd
jamesleekko <jamesleecode@gmail.com>
2023-06-28 18:44:15 +0800

    some development

--- src/components/paths/Index.vue
+++ src/components/paths/Index.vue
@@ -31,6 +31,7 @@ const addArticles = (page, size) => {
       if (!scrollWatched) {
         articleLeft.value = articleTotal.value - 1;
         await nextTick();
+        setContainerHeight();
         watchScroll();
       } else {
         articleLeft.value--;
@@ -41,7 +42,24 @@ const addArticles = (page, size) => {
 addArticles(currentPage.value, pageSize.value);
 
 const nextGroup = () => {
-  articlegourpCount.value = 1;
+  if (articleLeft.value > 0) {
+    currentPage.value++;
+    articleGroupCount.value = 0;
+    addArticles(currentPage.value, pageSize.value);
+  }
+};
+
+const setContainerHeight = () => {
+  const groupEmpty = groupLimit - articleGroupCount.value;
+  const increaseCount =
+    groupEmpty < articleLeft.value ? groupEmpty : articleLeft.value;
+  const oneHeight = document.querySelector(".article-item").offsetHeight;
+  const itemMarginTop = getComputedStyle(
+    document.querySelector(".article-item")
+  ).marginTop.replace("px", "");
+  const increaseHeight = increaseCount * (oneHeight + Number(itemMarginTop));
+  const container = document.querySelector(".index-list");
+  container.style.height = `${container.offsetHeight + increaseHeight}px`;
 };
 
 const watchScroll = () => {
@@ -65,7 +83,8 @@ const watchScroll = () => {
             Number(itemMarginTop) +
             listDoms.length * (oneHeight + Number(itemMarginTop)) +
             10 &&
-        articleGroupCount.value < groupLimit
+        articleGroupCount.value < groupLimit &&
+        currentPage.value < articleTotal.value
       ) {
         currentPage.value++;
         addArticles(currentPage.value, pageSize.value);
@@ -116,8 +135,8 @@ onMounted(() => {
       </div>
     </div>
 
-    <div class="flex justify-center" v-if="articleGroupCount == 10">
-      <button class="draw-border">More</button>
+    <div class="flex justify-center" v-if="articleGroupCount === 10">
+      <button class="draw-border" @click="nextGroup()">More</button>
     </div>
   </div>
 </template>
